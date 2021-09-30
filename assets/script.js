628d0477f948dbb03a9fc03664d39c65859fab73
Matthew Flanagan <mattflanagan40@gmail.com>
2021-09-30 00:05:00 -0400

    comments removed from script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,3 @@
-// var body = document.querySelector('body');
-
-// var divEl = $('<div>');
-
-// divEl.addClass('textArea');
-
-// body.append(divEl);
-
-// console.log('here')
-
-// var obj = {
-//     "flammable": "inflammable",
-//     "duh": "no duh"
-//   };
-//   $.each( obj, function( key, value ) {
-//     alert( key + ": " + value );
-//   });
 
 //display current day & time.
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
@@ -65,23 +48,3 @@ $("#9am .description").val(localStorage.getItem("5pm"));
 
 currentTime();
 }
-
-
-
-// var container = $('.container')
-
-// var divTimeblock = ['<div class="row time-block"><div class="col-sm-2 hour">9am</div><textarea class="col-sm-8 description"></textarea><button class="col-sm-2 saveBtn">Save</button></div>'];
-
-// container.append(divTimeblock);
-
-// Make the hour dynamic
-
-// Make the time on the textArea dynamic depending on if it is in the past present or future
-
-// Allow user to type in textArea & when they click on Save Button they can save it on local Storage
-
-// Use local storage Getitem to retreive data from local storage 
-
-// Save the data only when save button is clicked 
-
-
